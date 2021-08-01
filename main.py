a3b11a4af099f022c8d57954f3417718f203ca96
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-31 22:46:44 -0500

    update flask app

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ shark_csv_path = "../data/sharks/sharks_cleaned.csv"
 app = Flask(__name__)
 
 # Flask Routes
+
 @app.route("/")
+def indextest():
+  return "pizza"
+
+@app.route("/tracker")
 def index():
   return render_template("SharkTracker.html")
   
